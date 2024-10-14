9b58ba4275289fe1f2bea65302b94baa069d207f
ukkokalevala <148769692+ukkokalevala@users.noreply.github.com>
2024-10-14 10:19:48 +0200

    Create secret.h

---
+++ secret.h
@@ -0,0 +1,11 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// Wi-Fi credentials
+const char* ssid = "yourSSID";        // Replace with your Wi-Fi SSID
+const char* password = "yourPASSWORD";   // Replace with your Wi-Fi password
+
+// OpenWeatherMap API Key
+const char* apiKey = "yourAPIKEY";    // Replace with your OpenWeatherMap API Key
+
+#endif
